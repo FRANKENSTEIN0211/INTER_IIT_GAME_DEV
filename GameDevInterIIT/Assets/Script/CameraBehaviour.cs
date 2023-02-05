3b2e0f8790ffe755238f7ffd7533e4a363eac701
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2023-02-06 00:45:12 +0530

    Added camera behavior to sample scene and added FOV adjustment with scroll wheel

--- GameDevInterIIT/Assets/Script/CameraBehaviour.cs
+++ GameDevInterIIT/Assets/Script/CameraBehaviour.cs
@@ -7,20 +7,31 @@ public class CameraBehaviour : MonoBehaviour
 {
     public CinemachineVirtualCamera cam;
     private CinemachineTransposer transposer;
-    public Vector3[] followOffsets = {new Vector3(0, 5, -9), new Vector3(0, 8, -1)};
+    public GameObject Player;
+    public Vector3[] followOffsets = {new Vector3(0, 7, -9), new Vector3(0, 8, -1)};
+    public float FOV = 40f;
     bool topDownCamera = false;
-    public float lerpSpeed = 10f;
+    public float lerpSpeed = 1f;
+    public float ScrollIncrement = 1f;
+    public float minFOV = 20f, maxFOV = 60f;
     void Start()
     {
         cam = gameObject.GetComponent<CinemachineVirtualCamera>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        cam.Follow = Player.transform;
+        cam.LookAt = Player.transform;
+        cam.m_Lens.FieldOfView = FOV;
         transposer = cam.GetCinemachineComponent<CinemachineTransposer>();
     }
     
     void Update(){
         if(Input.GetKeyDown("v")) topDownCamera = !topDownCamera;
+        FOV = Mathf.Clamp(FOV, minFOV, maxFOV);
+        FOV += -Input.mouseScrollDelta.y * ScrollIncrement;
     }
 
     void LateUpdate(){
+        cam.m_Lens.FieldOfView = Mathf.Lerp(cam.m_Lens.FieldOfView, FOV, lerpSpeed * Time.deltaTime);
         int index = (topDownCamera ? 1 : 0);
         transposer.m_XDamping = topDownCamera ? 0 : 1.2f;
         transposer.m_FollowOffset = Vector3.Lerp(transposer.m_FollowOffset, followOffsets[index], lerpSpeed * Time.deltaTime);

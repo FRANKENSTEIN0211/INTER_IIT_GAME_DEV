4dbc18a9eb7f1c893b6901cfb649db4c2d353d44
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2023-02-06 20:34:39 +0530

    made camera and movement system

--- GameDevInterIIT/Assets/Script/PlayerController.cs
+++ GameDevInterIIT/Assets/Script/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform followTarget;
+    public Rigidbody rb;
+    public Vector2 input = Vector2.zero;
+    public Vector3 moveDir = Vector3.zero;
+
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 5f;
+    public Vector3 followTargetRotation;
+    public float minAngle = -60f, maxAngle = 85f;
     void Start()
     {
-        
+        followTargetRotation = Vector3.zero;
+        rb = gameObject.GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input.Normalize();
         
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        followTargetRotation.y += mouseX * rotateSpeed * Time.deltaTime;
+        followTargetRotation.x -= mouseY * rotateSpeed * Time.deltaTime;    
+        followTargetRotation.x = Mathf.Clamp(followTargetRotation.x, minAngle, maxAngle);
     }
+
+    void FixedUpdate()
+    {
+        followTarget.eulerAngles = followTargetRotation;
+        moveDir = (followTarget.forward * input.y + followTarget.right * input.x).normalized;
+        moveDir.y = 0f;
+        rb.velocity = moveDir * moveSpeed * Time.fixedDeltaTime;
+    }
+
 }
